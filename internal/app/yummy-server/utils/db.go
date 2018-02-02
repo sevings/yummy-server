f5d10306b65a8489c55b06d376d17f5a0039fa21
binque <binque@ya.ru>
2018-02-02 21:07:14 +0300

    db port

--- internal/app/yummy-server/utils/db.go
+++ internal/app/yummy-server/utils/db.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"database/sql"
 	"log"
+	"strconv"
 
 	"github.com/go-openapi/runtime/middleware"
 	goconf "github.com/zpatrick/go-config"
@@ -24,7 +25,7 @@ func ClearDatabase(db *sql.DB) {
 	}
 
 	dropTable(tx, "entries_privacy")
-	dropTable(tx, "entry_tags")
+	// dropTable(tx, "entry_tags")
 	dropTable(tx, "entry_votes")
 	dropTable(tx, "entries")
 	dropTable(tx, "comment_votes")
@@ -59,6 +60,11 @@ func OpenDatabase(config *goconf.Config) *sql.DB {
 		log.Print(err)
 	}
 
+	port, err := config.Int("database.port")
+	if err != nil {
+		log.Print(err)
+	}
+
 	user, err := config.String("database.user")
 	if err != nil {
 		log.Print(err)
@@ -74,7 +80,7 @@ func OpenDatabase(config *goconf.Config) *sql.DB {
 		log.Print(err)
 	}
 
-	db, err := sql.Open(driver, "user="+user+" password="+pass+" dbname="+name)
+	db, err := sql.Open(driver, "user="+user+" password="+pass+" dbname="+name+" port="+strconv.Itoa(port))
 	if err != nil {
 		log.Fatal(err)
 	}
