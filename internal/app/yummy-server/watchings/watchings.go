b2d67bbf3773947aa0b75a01d5de759490818fe2
Vasily <binque@ya.ru>
2018-02-12 12:12:59 +0300

    watches

--- internal/app/yummy-server/watchings/watchings.go
+++ internal/app/yummy-server/watchings/watchings.go
@@ -13,6 +13,8 @@ import (
 // ConfigureAPI creates operations handlers
 func ConfigureAPI(db *sql.DB, api *operations.YummyAPI) {
 	api.WatchingsGetEntriesIDWatchingHandler = watchings.GetEntriesIDWatchingHandlerFunc(newStatusLoader(db))
+	api.WatchingsPutEntriesIDWatchingHandler = watchings.PutEntriesIDWatchingHandlerFunc(newWatchingAdder(db))
+	api.WatchingsDeletetEntriesIDWatchingHandler = watchings.DeleteEntriesIDWatchingHandlerFunc(newWatchingDeleter(db))
 }
 
 func watchingStatus(tx *utils.AutoTx, userID, entryID int64) *models.WatchingStatus {
@@ -43,7 +45,7 @@ func newStatusLoader(db *sql.DB) func(watchings.GetEntriesIDWatchingParams, *mod
 	}
 }
 
-func AddWatching(tx *utils.AutoTx, userID, entryID int64) {
+func AddWatching(tx *utils.AutoTx, userID, entryID int64) *models.WatchingStatus {
 	const q = `
 		INSERT INTO watching(user_id, entry_id)
 		VALUES($1, $2)
@@ -51,12 +53,56 @@ func AddWatching(tx *utils.AutoTx, userID, entryID int64) {
 		DO NOTHING`
 
 	tx.Exec(q, userID, entryID)
+
+	status := models.WatchingStatus{
+		ID:         entryID,
+		IsWatching: true,
+	}
+
+	return &status
 }
 
-func RemoveWatching(tx *utils.AutoTx, userID, entryID int64) {
+func newWatchingAdder(db *sql.DB) func(watchings.PutEntriesIDWatchingParams, *models.UserID) middleware.Responder {
+	return func(params watchings.PutEntriesIDWatchingParams, uID *models.UserID) middleware.Responder {
+		return utils.Transact(db, func(tx *utils.AutoTx) middleware.Responder {
+			userID := int64(*uID)
+			canView := utils.CanViewEntry(tx, userID, params.ID)
+			if !canView {
+				return watchings.NewPutEntriesIDWatchingNotFound()
+			}
+
+			status := AddWatching(tx, userID, params.ID)
+			return watchings.NewPutEntriesIDWatchingOK().WithPayload(status)
+		})
+	}
+}
+
+func RemoveWatching(tx *utils.AutoTx, userID, entryID int64) *models.WatchingStatus {
 	const q = `
 		DELETE FROM watching
 		WHERE user_id = $1 AND entry_id = $2`
 
 	tx.Exec(q, userID, entryID)
+
+	status := models.WatchingStatus{
+		ID:         entryID,
+		IsWatching: false,
+	}
+
+	return &status
+}
+
+func newWatchingDeleter(db *sql.DB) func(watchings.DeleteEntriesIDWatchingParams, *models.UserID) middleware.Responder {
+	return func(params watchings.DeleteEntriesIDWatchingParams, uID *models.UserID) middleware.Responder {
+		return utils.Transact(db, func(tx *utils.AutoTx) middleware.Responder {
+			userID := int64(*uID)
+			canView := utils.CanViewEntry(tx, userID, params.ID)
+			if !canView {
+				return watchings.NewDeleteEntriesIDWatchingNotFound()
+			}
+
+			status := RemoveWatching(tx, userID, params.ID)
+			return watchings.NewDeleteEntriesIDWatchingOK().WithPayload(status)
+		})
+	}
 }
