aaa6c4bef3c3cd444070fec9ecf93142f166086c
binque <binque@ya.ru>
2018-06-20 18:23:41 +0300

    -

--- utils/server.go
+++ utils/server.go
@@ -63,8 +63,8 @@ func (srv *MindwellServer) NewCover(id int64, cover string) *models.Cover {
 
 	return &models.Cover{
 		ID:    id,
-		X1920: base + "cover/1920" + cover,
-		X318:  base + "cover/318" + cover,
+		X1920: base + "cover/1920/" + cover,
+		X318:  base + "cover/318/" + cover,
 	}
 }
 
