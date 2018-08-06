46c71a349bfa9bbb63229182d31d074182f116ef
Vasily <binque@ya.ru>
2018-08-06 09:27:57 +0300

    images sql

--- scripts/mindwell.sql
+++ scripts/mindwell.sql
@@ -1773,12 +1773,43 @@ CREATE TABLE "mindwell"."images" (
 	"id" Serial NOT NULL,
     "user_id" Integer NOT NULL,
 	"path" Text NOT NULL,
+    "mime" Text NOT NULL,
     "created_at" Timestamp With Time Zone DEFAULT CURRENT_TIMESTAMP NOT NULL);
  ;
 -- -------------------------------------------------------------
 
 -- CREATE INDEX "index_image_id" -------------------------------
 CREATE UNIQUE INDEX "index_image_id" ON "mindwell"."images" USING btree( "id" );
+-- -------------------------------------------------------------
+
+
+
+-- CREATE TABLE "size" -----------------------------------------
+CREATE TABLE "mindwell"."size" (
+    "id" Integer UNIQUE NOT NULL,
+    "type" Text NOT NULL );
+
+INSERT INTO "mindwell"."size" VALUES(0, 'small');
+INSERT INTO "mindwell"."size" VALUES(1, 'medium');
+INSERT INTO "mindwell"."size" VALUES(2, 'large');
+-- -------------------------------------------------------------
+
+
+
+-- CREATE TABLE "image_sizes" ----------------------------------
+CREATE TABLE "mindwell"."image_sizes" (
+    "image_id" Integer NOT NULL,
+    "size" Integer NOT NULL,
+    "width" Integer NOT NULL,
+    "height" Integer NOT NULL,
+    CONSTRAINT "unique_image_size" PRIMARY KEY ("image_id", "size"),
+    CONSTRAINT "unique_image_id" FOREIGN KEY ("image_id") REFERENCES "mindwell"."images"("id"),
+    CONSTRAINT "enum_image_size" FOREIGN KEY("size") REFERENCES "mindwell"."size"("id")
+);
+-- -------------------------------------------------------------
+
+-- CREATE INDEX "index_image_size_id" --------------------------
+CREATE INDEX "index_image_size_id" ON "mindwell"."image_sizes" USING btree( "image_id" );
 -- -------------------------------------------------------------
 
 
