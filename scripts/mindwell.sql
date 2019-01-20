fc05dd0caa30a09c2cbdea7ab7002a98523b5f67
binque <binque@ya.ru>
2019-01-20 15:07:49 +0300

    give_invites

--- scripts/mindwell.sql
+++ scripts/mindwell.sql
@@ -1868,13 +1868,17 @@ CREATE OR REPLACE FUNCTION give_invites() RETURNS VOID AS $$
     WITH inviters AS (
         UPDATE mindwell.users 
         SET last_invite = CURRENT_DATE
-        WHERE (rank <= (
-                    SELECT COUNT(DISTINCT author_id) / 2
-                    FROM mindwell.entries
-                    WHERE age(created_at) <= '2 months' 
-                        AND visible_for = (SELECT id FROM entry_privacy WHERE type = 'all')
+        WHERE ((id IN (
+                    SELECT author_id 
+                    FROM (
+                        SELECT created_at, author_id, rating 
+                        FROM entries 
+                        WHERE age(created_at) <= interval '1 month' 
+                            AND visible_for = (SELECT id FROM entry_privacy WHERE type = 'all')
+                        ORDER BY rating DESC 
+                        LIMIT 100) AS e
+                    WHERE created_at::date = CURRENT_DATE - 3
                 )
-                AND age(last_invite) >= interval '7 days'
                 AND (SELECT COUNT(*) FROM mindwell.invites WHERE referrer_id = users.id) < 3
             ) OR (
                 last_invite = created_at::Date
@@ -1884,7 +1888,7 @@ CREATE OR REPLACE FUNCTION give_invites() RETURNS VOID AS $$
                     WHERE entries.author_id = users.id AND entry_votes.entry_id = entries.id
                         AND entry_votes.vote > 0 AND entry_votes.user_id <> users.invited_by
                 ) >= 10
-            )
+            )) AND age(last_invite) >= interval '14 days'
         RETURNING users.id
     ), wc AS (
         SELECT COUNT(*) AS words FROM invite_words
