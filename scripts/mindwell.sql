f56e12bc4a34b70351c0e1bff867a3ea0b4ec31f
Vasily <binque@ya.ru>
2019-09-20 11:33:53 +0300

    fix invited count

--- scripts/mindwell.sql
+++ scripts/mindwell.sql
@@ -161,19 +161,41 @@ CREATE INDEX "index_user_search" ON "mindwell"."users" USING GIST
     (to_search_string("name", "show_name", "country", "city") gist_trgm_ops);
 -- -------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION mindwell.count_invited() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION mindwell.count_invited_upd() RETURNS TRIGGER AS $$
     BEGIN
+        IF (OLD.invited_by = NEW.invited_by) THEN
+            RETURN NULL;
+        END IF;
+
         UPDATE mindwell.users
-        SET invited_count = invited_count + 1 
+        SET invited_count = invited_count + 1
         WHERE id = NEW.invited_by;
 
         RETURN NULL;
     END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER cnt_invited
-    AFTER INSERT ON mindwell.users
-    FOR EACH ROW EXECUTE PROCEDURE mindwell.count_invited();
+CREATE TRIGGER cnt_invited_upd
+    AFTER UPDATE ON mindwell.users
+    FOR EACH ROW EXECUTE PROCEDURE mindwell.count_invited_upd();
+
+CREATE OR REPLACE FUNCTION mindwell.count_invited_del() RETURNS TRIGGER AS $$
+    BEGIN
+        IF (OLD.invited_by IS NULL) THEN
+            RETURN NULL;
+        END IF;
+
+        UPDATE mindwell.users
+        SET invited_count = invited_count - 1
+        WHERE id = OLD.invited_by;
+
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER cnt_invited_del
+    AFTER DELETE ON mindwell.users
+    FOR EACH ROW EXECUTE PROCEDURE mindwell.count_invited_del();
 
     
 
